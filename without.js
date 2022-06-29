abe7e33899e57eae4ec0365938638facd0826c97
jadenlee0519 <jadenlee0519@gmail.com>
2022-06-28 18:13:49 -0700

    practice for without

---
+++ without.js
@@ -0,0 +1,37 @@
+const assertArraysEqual = function (arr1, arr2) {
+  const sucessMsg = `🟢 🟢 🟢 Arguments match: ${arr1} === ${arr2} 🟢 🟢 🟢 `;
+  const failMsg = `🔴 🔴 🔴 Arguments DO NOT match: ${arr1} === ${arr2} 🔴 🔴 🔴 `;
+  let isEqual = false;
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        isEqual = true;
+      } else if (arr1[i] !== arr2[i]) {
+        console.log(failMsg);
+        return;
+      }
+    }
+  }
+  if (isEqual) {
+    console.log(sucessMsg);
+  } else {
+    console.log(failMsg);
+  }
+};
+
+const without = function (source, itemsToRemove) {
+  let newArr = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      newArr.push(source[i])
+    }
+  }
+  //console.log(source) --chekc if original array is modified
+  return newArr
+}
+
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
