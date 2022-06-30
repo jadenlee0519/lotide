aba9fd68b60f58ef605691549c998ba01789a555
jadenlee0519 <jadenlee0519@gmail.com>
2022-06-29 20:02:26 -0700

    findkeybyValue

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢 🟢 🟢 Assertion Passed: ${actual} === ${expected} 🟢 🟢 🟢 `);
+  } else {
+    console.log(`🔴 🔴 🔴 Assertion Failed: ${actual} !== ${expected} 🔴 🔴 🔴 `);
+  }
+};
+
+const findKeyByValue = function(obj, value) {
+  let keys = Object.keys(obj);
+  for (let i of keys) {
+   if (obj[i] === value) {
+     return i;
+   }
+ }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined); 
