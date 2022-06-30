c8db4366200b902234f4c8abb4b1ab3284714d62
jadenlee0519 <jadenlee0519@gmail.com>
2022-06-29 20:17:35 -0700

    eqObjects

---
+++ eqObjects.js
@@ -0,0 +1,48 @@
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = (object1, object2) => {
+  let obOne = Object.keys(object1);
+  let obTwo = Object.keys(object2);
+
+  if (obOne.length !== obTwo.length) {
+    return false;
+  } else {
+    for (let i in object1) {
+      if (Array.isArray(object1[i]) && Array.isArray(object2[i])) {
+        if (eqArrays(object1[i], object2[i]) === false) {
+          return false;
+        }
+      } else {
+        if (object1[i] !== object2[i]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+};
+
+
+
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(eqObjects(ab, ba)); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+console.log(eqObjects(ab, abc)); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, dc)); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(eqObjects(cd, cd2)); // => false 
