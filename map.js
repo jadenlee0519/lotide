5c4173b04446df040d70ba672e6fc3a4fdcfd956
jadenlee0519 <jadenlee0519@gmail.com>
2022-06-30 17:20:35 -0700

    learn how to use map

---
+++ map.js
@@ -0,0 +1,31 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = (array, callback) => {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+const firstLetter = map(words, word => word[0]);
+  console.log(firstLetter);
+  assertArraysEqual(firstLetter, ['g', 'c', 't', 'm', 't']); //Assertion Passed! Weeeee 
